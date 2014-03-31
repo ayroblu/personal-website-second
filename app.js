f82aa95546fd8d197fb379abfeebacd77606113c
Ben Lu <ben@ben-l.com>
2014-04-01 12:00:17 +1300

    Hopefully subdomains...

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express(); 
 
 app 
-.use(express.vhost('benlu.co', require('server.js').app)) 
-.use(express.vhost('a.benlu.co', require('a.server.js').app)) 
+.use(express.vhost('benlu.co', require('./server.js').app)) 
+.use(express.vhost('a.benlu.co', require('./a.server.js').app)) 
 .listen(80); 
